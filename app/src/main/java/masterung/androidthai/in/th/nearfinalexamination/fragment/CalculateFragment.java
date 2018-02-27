ef83a45e5b057360d6ba45e34aeecc16da851ee3
MasterUNG <phrombutr@gmail.com>
2018-02-27 11:03:56 +0700

    Back Controller

--- app/src/main/java/masterung/androidthai/in/th/nearfinalexamination/fragment/CalculateFragment.java
+++ app/src/main/java/masterung/androidthai/in/th/nearfinalexamination/fragment/CalculateFragment.java
@@ -3,10 +3,12 @@ package masterung.androidthai.in.th.nearfinalexamination.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import masterung.androidthai.in.th.nearfinalexamination.MainActivity;
 import masterung.androidthai.in.th.nearfinalexamination.R;
 
 /**
@@ -14,6 +16,30 @@ import masterung.androidthai.in.th.nearfinalexamination.R;
  */
 
 public class CalculateFragment extends Fragment{
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        Create Toolbar
+        Toolbar toolbar = getView().findViewById(R.id.toolbarCalculate);
+        ((MainActivity) getActivity()).setSupportActionBar(toolbar);
+
+        ((MainActivity) getActivity()).getSupportActionBar().setHomeButtonEnabled(true);
+        ((MainActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().getSupportFragmentManager().popBackStack();
+            }
+        });
+
+
+//        Exchange Controller
+
+
+    }   // Main Method
 
     @Nullable
     @Override
